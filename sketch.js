4328cee0a17d1619b495047578413f3dbe07350f
727974 <104579284+727974@users.noreply.github.com>
2022-05-04 10:44:58 -0400

    Delete sketch.js

--- sketch.js
+++
@@ -1,9 +0,0 @@
-const numBox = document.querySelector("input#num")
-const dubBtn = document.querySelector("button#DoubleBtn")
-const dubNum = document.querySelector("span#dubNum")
-
-dubBtn.onclick = () => {
-  let num = numBox.value
-  let double = numBox.value*2
-  dubNum.innerHTML = double
-}
